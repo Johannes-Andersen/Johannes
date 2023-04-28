5f0d2066862452bd0cfd02bab6ad2fccf5e8502f
Johannes Andersen <jotoffen@gmail.com>
2023-04-28 21:18:09 +0200

    feat: add lanyard

---
+++ src/utils/fetchDiscordStatus.ts
@@ -0,0 +1,18 @@
+const fetchDiscordStatus = async () => {
+  const res = await fetch(
+    "https://api.lanyard.rest/v1/users/98172944773029888",
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch Discord status");
+  }
+
+  return res.json();
+};
+
+export default fetchDiscordStatus;
