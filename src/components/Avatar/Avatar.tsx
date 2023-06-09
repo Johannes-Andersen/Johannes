90761ea436a3ae59eaeb7a60ca2f267f593ce7c8
Johannes Andersen <jotoffen@gmail.com>
2023-06-09 23:33:39 +0200

    feat: use color blind accessible icons

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { MoonIcon } from '@heroicons/react/24/solid'
 import { FC } from 'react'
 
 interface Props {
@@ -8,31 +9,76 @@ interface Props {
   priority?: boolean
 }
 
-// TODO: Use accessible icons
-const statusMapping: Record<string, { color: string; humanizedName: string }> =
-  {
-    online: { color: 'bg-green-400', humanizedName: 'Online' },
-    offline: { color: 'bg-gray-400', humanizedName: 'Offline' },
-    idle: { color: 'bg-yellow-400', humanizedName: 'Idle' },
-    dnd: { color: 'bg-red-400', humanizedName: 'Do Not Disturb' },
-    streaming: { color: 'bg-purple-400', humanizedName: 'Streaming' },
+const statusMapping: Record<
+  string,
+  { color: string; humanizedName: string; icon?: JSX.Element }
+> = {
+  online: {
+    color: 'bg-green-400',
+    humanizedName: 'Online',
+  },
+  offline: { color: 'bg-gray-400', humanizedName: 'Offline' },
+  idle: {
+    color: 'bg-yellow-400',
+    humanizedName: 'Idle',
+    icon: (
+      <MoonIcon
+        aria-hidden="true"
+        className="h-4 w-4 -rotate-90 bg-white dark:bg-gray-800 text-yellow-400 absolute bottom-0 right-0 block rounded-full ring-2 ring-white dark:ring-gray-800"
+      />
+    ),
+  },
+  dnd: {
+    color: 'bg-red-400',
+    humanizedName: 'Do Not Disturb',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        className="h-4 w-4 bg-red-500 text-yellow-400 absolute bottom-0 right-0 block rounded-full ring-2 ring-white dark:ring-gray-800"
+        viewBox="0 0 304.22 304.22"
+      >
+        <path
+          d="M226.92 138.26H77.31a13.85 13.85 0 1 0 0 27.7h149.61a13.85 13.85 0 1 0 0-27.7z"
+          style={{ fill: 'black' }}
+        />
+      </svg>
+    ),
+  },
+  streaming: { color: 'bg-purple-400', humanizedName: 'Streaming' },
+}
+
+const IconToUse = ({ status }: { status: string }) => {
+  const statusInfo = statusMapping[status]
+  if (statusInfo.icon) {
+    return statusInfo.icon
   }
 
-const DiscordStatus: FC<Props> = ({ img, status, priority, username }) => (
-  <span className="relative inline-block">
-    <Image
-      width={64}
-      height={64}
-      className="h-16 w-16 rounded-full"
-      src={img}
-      alt={`${username}, ${statusMapping[status].humanizedName}`}
-      priority={priority}
-    />
+  if (status === 'offline') return null
+
+  return (
     <span
-      className={`absolute bottom-0 right-0 block h-4 w-4 rounded-full ring-2 ring-white ${statusMapping[status].color}
+      aria-hidden="true"
+      className={`absolute bottom-0 right-0 block h-4 w-4 rounded-full ring-2 ring-white dark:ring-gray-800 ${statusMapping[status].color}
       `}
     />
-  </span>
-)
+  )
+}
+
+const DiscordStatus: FC<Props> = ({ img, status, priority, username }) => {
+  return (
+    <span className="relative inline-block">
+      <Image
+        width={64}
+        height={64}
+        className="h-16 w-16 rounded-full"
+        src={img}
+        alt={`${username}, ${statusMapping[status].humanizedName}`}
+        priority={priority}
+      />
+      <IconToUse status={status} />
+    </span>
+  )
+}
 
 export default DiscordStatus
