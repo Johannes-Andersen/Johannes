4fe0d5ca6ae56d0cec78478d19a08b957f14f0fb
Johannes Andersen <jotoffen@gmail.com>
2023-06-09 22:06:04 +0200

    feat: style 404 page to darkmode

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,15 @@
 const NotFound = () => (
-  <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+  <main className="grid min-h-full place-items-center bg-white dark:bg-gray-800 px-6 py-24 sm:py-32 lg:px-8">
     <div className="text-center">
       <p className="text-base font-semibold text-brand-600">404</p>
-      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
         Houston, we&apos;ve got a problem!
       </h1>
-      <p className="mt-6 text-base leading-7 text-gray-600">
+      <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-100">
         Our trusty space explorer couldn&apos;t locate the cosmic destination
         you seek.
       </p>
-      <p className="text-base leading-7 text-gray-600">
+      <p className="text-base leading-7 text-gray-600 dark:text-gray-100">
         Fear not, intrepid adventurer!
       </p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
