074588d18d025e78967e15b335a94c200a4f9a45
Johannes Andersen <jotoffen@gmail.com>
2023-09-07 03:14:52 +0200

    fix: use spec compliant nonce in csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,42 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers)
-  let cspValue =
-    "default-src 'self'; " +
-    "frame-ancestors 'none'; " +
-    'frame-src https://challenges.cloudflare.com; ' +
-    "form-action 'self'; " +
-    "connect-src 'self' cloudflareinsights.com wss://api.lanyard.rest; " +
-    "base-uri 'self'; " +
-    "object-src 'none'; " +
-    "style-src 'self'; " +
-    'upgrade-insecure-requests; block-all-mixed-content; sandbox allow-forms allow-same-origin allow-scripts allow-top-navigation allow-popups; ' +
-    'report-uri https://johand.report-uri.com/r/d/csp/enforce; ' +
-    `script-src 'self' challenges.cloudflare.com ajax.cloudflare.com static.cloudflareinsights.com ${
+export function middleware(_request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' challenges.cloudflare.com ajax.cloudflare.com static.cloudflareinsights.com ${
       process.env.NODE_ENV === 'development' ? " 'unsafe-eval'" : ''
-    }`
+    } 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    connect-src 'self' cloudflareinsights.com wss://api.lanyard.rest;
+    report-uri https://johand.report-uri.com/r/d/csp/enforce;
+    frame-ancestors 'none';
+    frame-src https://challenges.cloudflare.com;
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+    sandbox allow-forms allow-same-origin allow-scripts allow-top-navigation allow-popups;
+`
 
-  const nonce = crypto.randomUUID()
+  const requestHeaders = new Headers()
+  requestHeaders.set('x-nonce', nonce)
+  requestHeaders.set(
+    'Content-Security-Policy',
+    // Replace newline characters and spaces
+    cspHeader.replace(/\s{2,}/g, ' ').trim()
+  )
 
-  cspValue += ` 'nonce-${nonce}' 'strict-dynamic'`
-
-  requestHeaders.set('content-security-policy', cspValue)
-
-  const response = NextResponse.next({
+  return NextResponse.next({
+    headers: requestHeaders,
     request: {
       headers: requestHeaders,
     },
   })
-
-  response.headers.set('content-security-policy', cspValue)
-  response.headers.set('x-csp-nonce', nonce)
-
-  return response
 }
 
 export const config = {
