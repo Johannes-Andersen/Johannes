9196bbaad425376c1bddedd2e43dcfd81b9d0dea
Johannes Andersen <jotoffen@gmail.com>
2023-12-29 00:54:03 +0100

    chore: cleanup of styles and unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,6 +22,7 @@ export default defineConfig({
       configFile: "./tailwind.config.ts",
     }),
     sitemap(),
+    icon(),
   ],
   site: "https://johand.dev",
 });
