758cf0a9cf3959c6b8321173749c2ce45a791d76
Johannes Andersen <jotoffen@gmail.com>
2023-12-29 02:15:09 +0100

    perf: better cache policy

---
+++ src/middleware/cacheHeaders.ts
@@ -0,0 +1,12 @@
+import { defineMiddleware } from "astro:middleware";
+
+const cacheHeaders = defineMiddleware(async (_, next) => {
+  const response = await next();
+  const { headers } = response;
+
+  headers.set("Cache-Control", "must-revalidate, max-age=0");
+
+  return response;
+});
+
+export default cacheHeaders;
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,8 @@
 import { sequence } from "astro:middleware";
 
 import securityHeaders from "./securityHeaders";
+import cacheHeaders from "./cacheHeaders";
 
-const middleware = () => sequence(securityHeaders);
+const middleware = () => sequence(securityHeaders, cacheHeaders);
 
 export const onRequest = middleware();
