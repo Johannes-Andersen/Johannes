bbd80876f99a9461f3821952a97ed1f13e3c9f4a
Johannes Andersen <jotoffen@gmail.com>
2024-07-27 00:55:33 +0200

    fix: try to use getSecret for CF build

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
-import { CLERK_PUBLISHABLE_KEY, CLERK_SECRET_KEY } from 'astro:env/server';
+import { CLERK_PUBLISHABLE_KEY, getSecret } from 'astro:env/server';
 import { clerkMiddleware } from '@clerk/astro/server';
+
+const CLERK_SECRET_KEY = getSecret('CLERK_SECRET_KEY');
 
 export const onRequest = clerkMiddleware({
   publishableKey: CLERK_PUBLISHABLE_KEY,
-  secretKey: CLERK_SECRET_KEY,
+  secretKey: CLERK_SECRET_KEY || '',
 });
