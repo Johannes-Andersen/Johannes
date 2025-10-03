2c84259786727a8d8c85a78627c047bf40778695
Johannes Andersen <johannes.andersen@telenor.no>
2025-10-03 13:41:02 +0200

    chore: fix build issues

--- astro.config.ts
+++ astro.config.ts
@@ -59,6 +59,7 @@ export default defineConfig({
     domains: ['johand.dev', 'cdn.bsky.app'],
   },
   vite: {
+    // @ts-expect-error
     plugins: [tailwindcss()],
     build: {
       sourcemap: true,
